482d3b8013bba3ff42a2cb72c27d781d8ae80d98
Kresnadc <kresna_dcahyo@yahoo.co.id>
2015-11-06 14:34:21 +0700

    Update Main.java
    Bug FIXED

--- src/game/Main.java
+++ src/game/Main.java
@@ -7,7 +7,7 @@ import java.util.LinkedList;
 public class Main {
 
     public static void main(String[] args) {
-        Game game = new Game("Kresna");
+        Game game = new Game("Pemain");
         game.start();
         while(!game.isGameOver()){
             game.play();
